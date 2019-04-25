da983f7ed5184d34d19039651754d982dd84e7d6
marcosprintes <marcosp.printes@hotmail.com>
2019-04-25 10:07:59 -0400

    ajuste acesso mysql

--- gomysql/main.go
+++ gomysql/main.go
@@ -12,7 +12,8 @@ func main() {
 	fmt.Println("go mysql tutorial loco")
 	// Open up our database connection.
 	// The database is called mytest
-	db, err := sql.Open("mysql", "root:13231989@tcp(127.0.0.1:3306)/test")
+	// db, err := sql.Open("mysql", "root:13231989@tcp(127.0.0.1:3306)/test")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/test")
 
 	if err != nil {
 		panic(err.Error())
@@ -20,11 +21,32 @@ func main() {
 
 	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO mytest VALUES ( 100, 'Marcos Paulo Alho Printes' )")
+	// insert, err := db.Query("INSERT INTO mytest VALUES ( 200, 'Mel' )")
+
+	// del, err := db.Query("DELETE FROM mytest")
+
+	//results, err := db.Query("SELECT * FROM mytest")
 
 	if err != nil {
 		panic(err.Error())
 	}
-	// be careful deferring Queries if you are using transactions
-	defer insert.Close()
+
+	for results.Next() {
+		var user User
+		err := results.Scan(&user.Id, &user.Name)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println("User => ", user)
+	}
+
+	//be careful deferring Queries if you are using transactions
+	defer results.Close()
+}
+
+type User struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
