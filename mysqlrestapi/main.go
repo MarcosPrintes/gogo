3c09603ecebcf1527da8a7112b89191051a9198c
marcosprintes <marcosp.printes@hotmail.com>
2019-04-29 17:48:07 -0400

    remove unsets

--- mysqlrestapi/main.go
+++ mysqlrestapi/main.go
@@ -1,3 +1,4 @@
+//https://medium.com/@kelvin_sp/building-and-testing-a-rest-api-in-golang-using-gorilla-mux-and-mysql-1f0518818ff6
 package main
 
 import (
@@ -11,7 +12,6 @@ import (
 	"os"
 )
 
-//https://medium.com/@kelvin_sp/building-and-testing-a-rest-api-in-golang-using-gorilla-mux-and-mysql-1f0518818ff6
 func main() {
 	var a = App{}
 	a.Initialize("root", "", "rest_api_example")
