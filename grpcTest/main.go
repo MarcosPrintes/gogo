213006ca0fdffe00fda6c48cb660d26320bd27c5
marcosprintes <marcosp.printes@hotmail.com>
2019-05-30 15:30:43 -0400

    grpc structure

---
+++ grpcTest/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com.br/MarcosPrintes/grpcTest/proto"
+	api "github.com.br/MarcosPrintes/grpcTest/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func init() {
+}
+
+func main() {
+
+	list, err := net.Listen("tcp", ":8088")
+
+	if err != nil {
+		log.Fatal("error start grpc server => ", err.Error())
+	}
+
+	server := api.Server{}
+
+	creds, err := credentials.NewServerTLSFromFile("cert/server.crt", "cert/server.key")
+	if err != nil {
+		log.Fatal("credentials error => ", err.Error())
+	}
+
+	//array options  with credentials
+	opts := []grpc.ServerOption{grpc.Creds(creds)}
+
+	fmt.Println("credentials options", opts)
+
+	// grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(opts...) // pass options to new server grpc
+	proto.RegisterPingServer(grpcServer, &server)
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatal("grpc server error credentials:", err.Error())
+	}
+}
--- grpcTest/server/server.go
+++ grpcTest/server/server.go
@@ -1,43 +1,20 @@
-package main
+package api
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net"
 
-	"github.com.br/MarcosPrintes/grpcTest/api"
 	"github.com.br/MarcosPrintes/grpcTest/proto"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
-func init() {
+type Server struct {
 }
 
-func main() {
+func NewServer() *Server {
+	return &Server{}
+}
 
-	list, err := net.Listen("tcp", ":8088")
-
-	if err != nil {
-		log.Fatal("error start grpc server => ", err.Error())
-	}
-
-	server := api.Server{}
-
-	creds, err := credentials.NewServerTLSFromFile("../cert/server.crt", "../cert/server.key")
-	if err != nil {
-		log.Fatal("credentials error => ", err.Error())
-	}
-
-	//array options  with credentials
-	opts := []grpc.ServerOption{grpc.Creds(creds)}
-
-	fmt.Println("credentials options", opts)
-
-	// grpcServer := grpc.NewServer()
-	grpcServer := grpc.NewServer(opts...) // pass options to new server grpc
-	proto.RegisterPingServer(grpcServer, &server)
-	if err := grpcServer.Serve(list); err != nil {
-		log.Fatal("grpc server error credentials:", err.Error())
-	}
+func (server *Server) PingMethod(ctx context.Context, msg *proto.PingMessage) (*proto.PingResponse, error) {
+	fmt.Println("message => ", msg)
+	return &proto.PingResponse{Response: "msg"}, nil
 }
